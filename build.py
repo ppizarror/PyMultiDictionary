aaa4351d700a8fa012c3ecd0e35e2e14d311b969
ppizarror <pablo@ppizarror.com>
2021-10-18 16:31:41 -0300

    Fix build

--- build.py
+++ build.py
@@ -8,26 +8,26 @@ BUILD.
 import os
 import sys
 
-assert len(sys.argv) == 2, 'Argument is required, usage: build.py pyinstaller/pip/twine/gource'
+assert len(sys.argv) == 2, 'Argument is required, usage: build.py pip/twine/gource'
 mode = sys.argv[1].strip()
 python = 'python3' if not sys.platform == 'win32' else 'py -3.7'
 
 if mode == 'pip':
-    if os.path.isdir('dist/pip'):
-        for k in os.listdir('dist/pip'):
+    if os.path.isdir('dist/'):
+        for k in os.listdir('dist/'):
             if 'PyMultiDictionary-' in k:
-                os.remove(f'dist/pip/{k}')
+                os.remove(f'dist/{k}')
     if os.path.isdir('build'):
         for k in os.listdir('build'):
             if 'bdist.' in k or k == 'lib':
                 os.system(f'rm -rf build/{k}')
-    os.system(f'{python} setup.py sdist --dist-dir dist/pip bdist_wheel --dist-dir dist/pip')
+    os.system(f'{python} setup.py sdist bdist_wheel')
 
 elif mode == 'twine':
-    if os.path.isdir('dist/pip'):
-        os.system(f'{python} -m twine upload dist/pip/*')
+    if os.path.isdir('dist/'):
+        os.system(f'{python} -m twine upload dist/*')
     else:
-        raise FileNotFoundError('Not distribution been found, execute build.py pip first')
+        raise FileNotFoundError('Not distribution been found, execute build.py pip')
 
 elif mode == 'gource':
     os.system('gource -s 0.25 --title PyMultiDictionary --disable-auto-rotate --key '

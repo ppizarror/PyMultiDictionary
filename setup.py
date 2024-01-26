8a995210d1461d737044c60e21068ccb952151d9
ppizarror <pablo@ppizarror.com>
2024-01-26 17:33:16 -0300

    Update pytest

--- setup.py
+++ setup.py
@@ -55,7 +55,7 @@ setup(
     install_requires=requirements,
     extras_require={
         'docs': ['sphinx<7', 'sphinx-autodoc-typehints>=1.2.0', 'sphinx-rtd-theme'],
-        'test': ['nose2[coverage_plugin]']
+        'test': ['nose2[coverage_plugin]', 'pytest']
     },
     setup_requires=[
         'setuptools',
